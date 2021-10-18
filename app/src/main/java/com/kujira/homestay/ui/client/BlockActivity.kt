e0becbba1257019fbd046dbe4a9de4c32a08c1f6
phucbv98x3 <buiphucdep@gmail.com>
2021-10-18 10:13:10 +0700

    update

--- app/src/main/java/com/kujira/homestay/ui/client/BlockActivity.kt
+++ app/src/main/java/com/kujira/homestay/ui/client/BlockActivity.kt
@@ -1,9 +1,11 @@
 package com.kujira.homestay.ui.client
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.kujira.homestay.R
+import com.kujira.homestay.ui.all_login.login_new.LoginActivity
 
 class BlockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,6 @@ class BlockActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         super.onBackPressed()
-        finish()
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
